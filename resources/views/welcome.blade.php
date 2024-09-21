9e67c35357f307ec89a8cf6d74a73b1d465da033
Salem Bahri <bahrisalem5@gmail.com>
2024-09-21 12:01:10 +0100

    rgrgrr

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,6 +17,8 @@
     </head>
     <body class="antialiased">
         <h1 style="color: #e5e7eb">Salem bahri Here</h1>
+        xsfwdfdf
+        xfdf
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
             @if (Route::has('login'))
                 <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
