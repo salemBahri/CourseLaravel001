b32f3d2830b6ded8f8b688ff2da42740b69a3533
Salem Bahri <bahrisalem5@gmail.com>
2024-09-22 00:40:10 +0100

    add a column to a table after creation

--- database/migrations/2024_09_21_170758_create_posts_table.php
+++ database/migrations/2024_09_21_170758_create_posts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('body');
-            $table->string('image')->nullable();
+            //$table->string('image')->nullable();
             //$table->unsignedBigInteger('user_id');
             $table->foreignId('user_id');
             //$table->bigInteger('category_id')->unsigned();
