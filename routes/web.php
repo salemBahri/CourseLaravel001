37c03916babf1efa0a66c3206c5c000e8aed8eb5
Salem Bahri <bahrisalem5@gmail.com>
2024-09-23 21:45:41 +0100

    create PostController With resources

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AppController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,5 +14,21 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
 Route::get('/', [AppController::class,'welcome']);
+
+
+
+// //Read
+// Route::get( '/posts', [PostController::class,'allPosts']);
+// Route::get('/posts/{id}', [PostController::class,'getPost']);
+// //Create
+// Route::get('/posts/create', [PostController::class,'createPostForm']);
+// Route::post('/posts', [PostController::class,'createPost']);
+// //Edit
+// Route::get('/posts/{id}/edit', [PostController::class,'editPostForm']);
+// Route::put('/posts/{id}', [PostController::class,'editPost']);
+// //Delete
+// Route::delete('/posts/{id}', [PostController::class,'deletePost']);
+
+
+Route::resource('/posts', PostController::class);
